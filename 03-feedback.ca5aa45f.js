bc6d5dfb1ccf4926e37badcc10d9b4481528def5
Olexii-US <106531559+Olexii-US@users.noreply.github.com>
2022-10-26 12:17:22 +0000

    Deploying to gh-pages from @ Olexii-US/goit-js-hw-08@2519c03dd8a4cd09932d3ab08b071e7bbfe6921b 🚀

--- 03-feedback.ca5aa45f.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form").addEventListener("submit",(function(e){e.preventDefault(),console.log(e.currentTarget)})),console.log("hi");
-//# sourceMappingURL=03-feedback.ca5aa45f.js.map
